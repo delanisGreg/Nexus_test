4c5e16eb6c7097b62bee53d7352d6108e94ca631
delanisGreg <63797913+delanisGreg@users.noreply.github.com>
2020-06-14 09:51:17 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
         name='helloworls',
         inclide_package_data=True,
-        version='0.test.1',
+        version='0.test.2',
         description='Hello!',
         py_module=["hello"],
         packages=['src'],
